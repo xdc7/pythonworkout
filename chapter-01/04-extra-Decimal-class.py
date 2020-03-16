f238d8cb1f90c8e4739fabb39d801e644c3b004e
Juzer Ali <juzerali@gmail.com>
2020-03-16 10:24:40 -0500

    adding script for chp1 ex4 extra 2

---
+++ chapter-01/04-extra-Decimal-class.py
@@ -0,0 +1,3 @@
+"""
+Explore the Decimal class (docs.python.org/3.7/library/decimal.html), which has an alternative floating-point representation that’s as accurate as any decimal number can be. Write a function that takes two strings from the user, turns them into Decimal instances, and then prints the floating-point sum of the user’s two inputs. In other words, make it possible for the user to enter 0.1 and 0.2, and for us to get 0.3 back.
+"""

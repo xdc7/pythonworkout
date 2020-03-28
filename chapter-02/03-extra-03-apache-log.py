5ef90ffb5ae920bb0b12b43ec5f64d5f94ea2ac5
Juzer Ali <juzerali@gmail.com>
2020-03-27 20:50:19 -0500

    completed 2.3.3

--- chapter-02/03-extra-03-apache-log.py
+++ chapter-02/03-extra-03-apache-log.py
@@ -2,3 +2,18 @@
 Read through an Apache logfile. If there is a 404 error — you can just search for ' 404 ', if you want — then display the IP address, which should be the first element.
 
 """
+import re
+notFoundRegex = re.compile(r'(\s404\s)')
+ipAddressRegex = re.compile(r'(\d+[.]\d+[.]\d+[.]\d+)')
+counter = 0
+with open ('access.log') as f:
+    for line in f.readlines():
+        # An alternate way to find the ' 404 ' string without using regex:
+        # if  ' 404 ' in line:
+        if notFoundRegex.search(line):
+            counter += 1
+            ipAddress = ipAddressRegex.search(line).group()
+            print (ipAddress)
+            # An alternate way to print the IP address without using regex:
+            # print (line.split()[0])
+print (f'number of 404s: {counter}')

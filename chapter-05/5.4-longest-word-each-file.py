80855047a9056b934b03b4d912f513dd78097f5b
Juzer Ali <juzerali@gmail.com>
2020-06-28 10:27:07 -0500

    Added an alternate solution using dict comprehension

--- chapter-05/5.4-longest-word-each-file.py
+++ chapter-05/5.4-longest-word-each-file.py
@@ -3,33 +3,57 @@ In this exercise, the user will provide the name of a directory. You are to find
 """
 
 import os, sys
-# directory = sys.path[0]
 
-# Function to return the absolute file paths of files in a directory
-def getAbsoluteFilePaths(directory):
-   for dirpath,_,filenames in os.walk(directory):
-       for f in filenames:
-           yield os.path.abspath(os.path.join(dirpath, f))
+# # Function to return the absolute file paths of files in a directory
+# def getAbsoluteFilePaths(directory):
+#    for dirpath,_,filenames in os.walk(directory):
+#        for f in filenames:
+#            yield os.path.abspath(os.path.join(dirpath, f))
 
-# Function to return the longest word in a file
-def findLongestWordInFile(filePath):
+# # Function to return the longest word in a file
+# def findLongestWordInFile(filePath):
+#     longestWord = ''
+#     with open(filePath) as f:
+#         for line in f.readlines():
+#             for word in line.split():
+#                 if len(word) > len(longestWord):
+#                     longestWord = word
+#     return longestWord
+
+# directory = input('Please provide a directory path....\n')
+# if not directory:
+#     directory = sys.path[0]
+
+# results = {}
+
+# for fileName in getAbsoluteFilePaths(directory):
+#     results[fileName] = findLongestWordInFile(fileName)
+
+
+# for k,v in results.items():
+#     print("file: {0:65} -- Longest word: {1:35}".format(k,v))
+
+## Alternative solution using dictionary comprehension and reading the file line by line
+
+def findLongestWordInFile(fileName):
     longestWord = ''
-    with open(filePath) as f:
-        for line in f.readlines():
-            for word in line.split():
-                if len(word) > len(longestWord):
-                    longestWord = word
+    for line in open(fileName):
+        for word in line.split():
+            if len(word) > len(longestWord):
+                longestWord = word
     return longestWord
 
-directory = input('Please provide a directory path....\n')
+
+directory = input('Input a directory name...\n')
+
 if not directory:
     directory = sys.path[0]
 
-results = {}
+# Print directly to the console
+# print ({fileName: findLongestWordInFile(os.path.join(directory,fileName)) for fileName in os.listdir(directory)}) 
 
-for fileName in getAbsoluteFilePaths(directory):
-    results[fileName] = findLongestWordInFile(fileName)
+# (sort of) Pretty print
+d = {fileName : findLongestWordInFile(os.path.join(directory, fileName)) for fileName in os.listdir(directory) if os.path.isfile(os.path.join(directory,fileName))}
 
-
-for k,v in results.items():
-    print("file: {0:65} -- Longest word: {1:35}".format(k,v))
+for k,v in d.items():
+    print("file: {0:40} -- longestWord: {1:25}".format(k,v))
